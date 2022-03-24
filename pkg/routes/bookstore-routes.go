9a7439d46d990cdcab2ded7e9cbc68d2a06f6d93
Hossein Hazratgholizadeh <hossein.hazratgholizade@gmail.com>
2022-03-24 02:45:20 +0200

    Create structure

---
+++ pkg/routes/bookstore-routes.go
@@ -0,0 +1,10 @@
+package routes
+
+import (
+	"github.com/gorilla/mux"
+	"github.com/Hosseinhgz/Go-bookstore-MYSQL-RestAPI/pkg/controllers"
+)
+
+var RegisterBookStoreRoutes = func (router *mux.Router){
+
+}
