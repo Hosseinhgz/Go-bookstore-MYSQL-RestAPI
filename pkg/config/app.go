1c53c1060d4adb5a632cae3ee4d67a647866691a
Hossein Hazratgholizadeh <hossein.hazratgholizade@gmail.com>
2022-03-24 06:53:27 +0200

    fix bugs
    The app is working properly

--- pkg/config/app.go
+++ pkg/config/app.go
@@ -10,8 +10,9 @@ var (
 )
 
 func Connect() {
-	// gorm.open("{name of database", "nameOfUser:password/{nameoftable}?")
-	d, err := gorm.Open("mysql", "bookstore:Hh44974497/book?charset=utf8&parseTime=True&loc=Local")
+
+	// dns format => dsn := "user:pass@tcp(127.0.0.1:3306)/dbname?charset=utf8mb4&parseTime=True&loc=Local")
+	d, err := gorm.Open("mysql", "root:@Hhh44974497@tcp(127.0.0.1:3307)/bookstore?charset=utf8mb4&parseTime=True&loc=Local")
 	if err != nil {
 		panic(err)
 	}
